3516dcc419c73a00f4c2b237a5ad6332c13d839f
robotzhang <445679586@qq.com>
2012-11-13 10:21:29 +0800

    注册密码sha512加密

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to request.env["HTTP_REFERER"] || root_url, :notice => '成功登录！'
+      session[:user] = @user
+      redirect_to request.env["HTTP_REFERER"] || root_url, :notice => '注册成功！'
     else
       render :new
     end
@@ -21,10 +22,10 @@ class UsersController < ApplicationController
   end
   def login
     @user = User.new(params[:user])
-    user = User.where(:email => @user.email, :password => @user.password).first
+    user = User.where(:email => @user.email, :password => Digest::SHA512.hexdigest(@user.password || '')).first
     if user
       session[:user] = user
-      redirect_to request.env["HTTP_REFERER"] || root_url, :notice => "登陆成功！"
+      redirect_to request.env["HTTP_REFERER"] || root_url, :notice => "登录成功！"
     else
       @user.errors[:account] = '用户名或密码错误！' unless @user.email.nil?
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :nickname, :email, :password,:password_confirmation
+  before_create :encryption
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+
+  private
+  def encryption
+    self.password = Digest::SHA512.hexdigest(self.password)
+  end
 end
