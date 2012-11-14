9fd5acd097c6edaf65f374ff35732e9a82b1bcc3
robotzhang <445679586@qq.com>
2012-11-14 18:24:29 +0800

    章节更新

--- app/controllers/admin/chapters_controller.rb
+++ app/controllers/admin/chapters_controller.rb
@@ -20,6 +20,14 @@ class Admin::ChaptersController < ApplicationController
     @chapter = Chapter.find(params[:id])
     render :template => 'admin/chapters/form'
   end
+  def update
+    @chapter = Chapter.find(params[:id])
+    if @chapter.update_attributes(params[:chapter])
+      redirect_to admin_chapters_url(:book_id => @chapter.book_id, :number => @chapter.number), :alert => '第'+@chapter.number.to_s+'章更新成功'
+    else
+      render :template => 'admin/chapters/form'
+    end
+  end
   def destroy
     @chapter = Chapter.find(params[:id])
     @book.destroy
--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -3,7 +3,7 @@ class Chapter < ActiveRecord::Base
   validates :number, :uniqueness => {
       :scope => :book_id, # 联合唯一验证
       :message => "章节%{value}已添加"
-  }
+  }, :on => 'create'
   validates_presence_of :title, :message => '标题不能为空'
   validates_presence_of :content, :message => '章节内容不能为空'
   validates_numericality_of :number, :message => '章节号只能为整数'
